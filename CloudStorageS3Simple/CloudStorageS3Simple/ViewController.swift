b4bc357c73df0ebb49a11ddf5cbfd28d8676ad1b
Alistair Cooper <cooper@swiftcodecomposer.com>
2016-07-25 08:42:24 -0700

    demo app for S3 storage blog

---
+++ CloudStorageS3Simple/CloudStorageS3Simple/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  CloudStorageS3Simple
+//
+//  Demo app for the "Cloud Storage for iOS Apps with AWS S3" Blog
+//  Demonstrating a simple image download from an S3 bucket
+//
+//  Created by Alistair Cooper on 7/25/16.
+//  Copyright © 2016 Alistair Cooper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // link for your object on S3
+    let url = NSURL(string: "https://s3-us-west-2.amazonaws.com/blogpoststorage07242016/acPiano.jpg")!
+    
+    var imageView: UIImageView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // initialize an image view
+        let ivFrame = CGRect(x: 0, y: 0, width: 300, height: 200)
+        imageView = UIImageView(frame: ivFrame)
+        
+        // download image from s3
+        let session = NSURLSession.sharedSession()
+        
+        let downloadTask = session.dataTaskWithURL(url) { data, response, error in
+            if data == nil {
+                print("\(error)")
+                
+            } else {
+                // update the UI on the main queue
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.imageView!.image = UIImage(data: data!)
+                    self.imageView!.contentMode = UIViewContentMode.ScaleAspectFit
+                    self.view.addSubview(self.imageView!)
+                }
+            }
+        }
+        
+        downloadTask.resume()
+    }
+}
+
